0467cad09c510a9b2e76122ef36c57b6ad2fb5d7
bilal-Bhatti-u2133923 <160130049+bilal-Bhatti-u2133923@users.noreply.github.com>
2024-03-14 12:47:21 +0000

    blockchain addition

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,13 +1,17 @@
 import { faker } from "@faker-js/faker";
+import { storeWeatherData } from "../helpers/helpers.js";
 
 export const generateLondonWeatherData = (): WeatherData => {
   // Generate random weather data
   const generatedWeatherData = {
+    city: "London",
     temperature: faker.number.int({ min: -15, max: 30 }),
     humidity: faker.number.int({ min: 79, max: 86 }),
     wind: faker.number.int({ min: 2, max: 78 }),
     rain: faker.number.int({ min: 65, max: 75 }),
   };
+
+  storeWeatherData(generatedWeatherData).catch(console.error)
 
   // Return weather data
   return generatedWeatherData;
@@ -16,11 +20,14 @@ export const generateLondonWeatherData = (): WeatherData => {
 export const generateDublinWeatherData = (): WeatherData => {
   // Generate random weather data
   const generatedWeatherData: WeatherData = {
+    city: "Dublin",
     temperature: faker.number.int({ min: -15, max: 30 }),
     humidity: faker.number.int({ min: 79, max: 86 }),
     wind: faker.number.int({ min: 2, max: 78 }),
     rain: faker.number.int({ min: 65, max: 75 }),
   };
+
+  storeWeatherData(generatedWeatherData).catch(console.error)
 
   // Return weather data
   return generatedWeatherData;
